75aa9375c7991ec5c0164e561a266f795f4ee768
panda.teh@tuta.io <panda.teh@tuta.io>
2023-07-12 14:41:07 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,13 @@
 import os
+from pathlib import Path
+
+Path("/my/directory").mkdir(parents=True, exist_ok=True)
 
 WORKING_DIR = "/root/work"
 PORTAPPS_DIR = "Apps"
 PORTDATA_DIR = "Data"
 
-os.mkdir(os.path.join(WORKING_DIR, PORTAPPS_DIR))
-os.mkdir(os.path.join(WORKING_DIR, PORTDATA_DIR))
+Path(os.path.join(WORKING_DIR, PORTAPPS_DIR)).mkdir(parents=True, exist_ok=True)
+Path(os.path.join(WORKING_DIR, PORTDATA_DIR)).mkdir(parents=True, exist_ok=True)
+
 print("Installation Done.")
