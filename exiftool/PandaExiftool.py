827615c8f4a90ec0ea0a47e91524b121e4d1eed3
panda.teh@tuta.io <panda.teh@tuta.io>
2023-07-12 17:27:01 +0800

    Update exiftool impl

--- exiftool/PandaExiftool.py
+++ exiftool/PandaExiftool.py
@@ -1,16 +1,30 @@
+import os
 from pathlib import Path
 import tarfile
+
+ALIAS_IND_STR = "# PANDA_CLOUD_VPS - Exiftool Aliases (AUTO GENERATED)"
+
+def registerAliases():
+  isExiftoolAliasesRegistered = True;
+  with open(Path().home() / ".bashrc", "r") as f:
+    bashrcContent = f.read()
+    if bashrcContent.find(ALIAS_IND_STR) < 0:
+      isExiftoolAliasesRegistered = False
+
+  if not isExiftoolAliasesRegistered:
+    with open(Path().home() / ".bashrc", "a") as f:
+      f.write("\n")
+      f.write(ALIAS_IND_STR + "\n")
+      f.write("alias exiftool='/root/work/Apps/Exiftool/Image-ExifTool-11.92/exiftool'\n")
+      f.write("alias exiftool-clr='/root/work/Apps/Exiftool/Image-ExifTool-11.92/exiftool -all= -overwrite_original -r'\n")
 
 def startInstall():
   print("Installing [exiftool]...")
 
   tarball = tarfile.open('exiftool/Image-ExifTool-11.92.tar.gz')
-  tarball.extractall('/root/work/App/Exiftool')
+  tarball.extractall('/root/work/Apps/Exiftool')
   tarball.close()
 
-  with open(Path().home() / ".bashrc", "a") as f:
-    f.write("\n")
-    f.write("alias exiftool='/root/work/App/Exiftool/Image-ExifTool-11.92/exiftool'\n")
-    f.write("alias exiftool-clr='/root/work/App/Exiftool/Image-ExifTool-11.92/exiftool -all= -overwrite_original -r'\n")
+  registerAliases()
 
   print("Installing [exiftool] DONE")
--- install.py
+++ install.py
@@ -2,31 +2,32 @@ import os
 from pathlib import Path
 from exiftool import PandaExiftool
 
-ALIAS_INDICATOR_STR = "# Panda CLOUD_VPS Custom Aliases"
+# ALIAS_INDICATOR_STR = "# Panda CLOUD_VPS Custom Aliases"
 WORKING_DIR = "/root/work"
 PORTAPPS_DIR = "Apps"
 PORTDATA_DIR = "Data"
+VPSCLOUD_ALIAS_DIR = "vpscloud_bash_aliases"
 
 Path(os.path.join(WORKING_DIR, PORTAPPS_DIR)).mkdir(parents=True, exist_ok=True)
 Path(os.path.join(WORKING_DIR, PORTDATA_DIR)).mkdir(parents=True, exist_ok=True)
+Path(os.path.join(WORKING_DIR, VPSCLOUD_ALIAS_DIR)).mkdir(parents=True, exist_ok=True)
 
-isCloudAliasesRegistered = True;
+# isCloudAliasesRegistered = True;
+# with open(Path().home() / ".bashrc", "r") as f:
+#     bashrcContent = f.read()
+#     if bashrcContent.find(ALIAS_INDICATOR_STR) < 0:
+#       isCloudAliasesRegistered = False
+# if not isCloudAliasesRegistered:
+#   with open(Path().home() / ".bashrc", "a") as f:
+#     f.write("\n")
+#     f.write(ALIAS_INDICATOR_STR + "\n")
+#     f.write("if [ -d $HOME/work/" + VPSCLOUD_ALIAS_DIR + " ]; then\n")
+#     f.write("\tfor f in $HOME/work/" + VPSCLOUD_ALIAS_DIR + "/*; do\n")
+#     f.write("\t\t. \"$f\";\n")
+#     f.write("\tdone\n")
+#     f.write("fi\n")
 
-with open(Path().home() / ".bashrc", "r") as f:
-    bashrcContent = f.read()
-    if bashrcContent.find(ALIAS_INDICATOR_STR) < 0:
-      isCloudAliasesRegistered = False
-
-if not isCloudAliasesRegistered:
-  with open(Path().home() / ".bashrc", "a") as f:
-    f.write("\n")
-    f.write(ALIAS_INDICATOR_STR + "\n")
-    f.write("if [ -f ~/.vpscloud_bash_aliases ]; then\n")
-    f.write("\t. ~/.vpscloud_bash_aliases\n")
-    f.write("fi\n")
-
-#PandaExiftool.startInstall()
-
+PandaExiftool.startInstall()
 print("Installation Done.")
 
 # Reload the BASH at the end of installation
