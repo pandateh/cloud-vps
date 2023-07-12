53a8d31977f91a7c606c098cb2b11824a72e9766
panda.teh@tuta.io <panda.teh@tuta.io>
2023-07-12 15:50:18 +0800

    Update exiftool

--- exiftool/PandaExiftool.py
+++ exiftool/PandaExiftool.py
@@ -1,8 +1,16 @@
+from pathlib import Path
 import tarfile
 
 def startInstall():
   print("Installing [exiftool]...")
-  file = tarfile.open('exiftool/Image-ExifTool-11.92.tar.gz')
-  file.extractall('/root/work/App/Exiftool')
-  file.close()
+
+  tarball = tarfile.open('exiftool/Image-ExifTool-11.92.tar.gz')
+  tarball.extractall('/root/work/App/Exiftool')
+  tarball.close()
+
+  with open(Path().home() / ".bashrc", "a") as f:
+    f.write("\n")
+    f.write("alias exiftool='/root/work/App/Exiftool/Image-ExifTool-11.92/exiftool'\n")
+    f.write("alias exiftool-clr='/root/work/App/Exiftool/Image-ExifTool-11.92/exiftool -all= -overwrite_original -r'\n")
+
   print("Installing [exiftool] DONE")
--- install.py
+++ install.py
@@ -6,8 +6,12 @@ WORKING_DIR = "/root/work"
 PORTAPPS_DIR = "Apps"
 PORTDATA_DIR = "Data"
 
-#Path(os.path.join(WORKING_DIR, PORTAPPS_DIR)).mkdir(parents=True, exist_ok=True)
-#Path(os.path.join(WORKING_DIR, PORTDATA_DIR)).mkdir(parents=True, exist_ok=True)
+Path(os.path.join(WORKING_DIR, PORTAPPS_DIR)).mkdir(parents=True, exist_ok=True)
+Path(os.path.join(WORKING_DIR, PORTDATA_DIR)).mkdir(parents=True, exist_ok=True)
+
 PandaExiftool.startInstall()
 
 print("Installation Done.")
+
+# Reload the BASH at the end of installation
+os.system("exec bash")
